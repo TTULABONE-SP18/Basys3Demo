4ed61a4c1099c977c27658730e1eccb9b1038abd
Derek Johnston <derek.a.johnston@ttu.edu>
2018-01-25 09:57:55 -0600

    Fixing a syntax error in TestFullAdder.v (C should be Cin)

--- TestBenches/TestFullAdder.v
+++ TestBenches/TestFullAdder.v
@@ -23,40 +23,40 @@ FullAdder uut (
 initial begin
   A = 1'b0;
   B = 1'b0;
-  C = 1'b0;
+  Cin = 1'b0;
   #20;
   A = 1'b0;
   B = 1'b0;
-  C = 1'b1;
+  Cin = 1'b1;
   #20;
   A = 1'b0;
   B = 1'b1;
-  C = 1'b0;
+  Cin = 1'b0;
   #20;
   A = 1'b0;
   B = 1'b1;
-  C = 1'b1;
+  Cin = 1'b1;
   #20;
   A = 1'b1;
   B = 1'b0;
-  C = 1'b0;
+  Cin = 1'b0;
   #20;
   A = 1'b1;
   B = 1'b0;
-  C = 1'b1;
+  Cin = 1'b1;
   #20;
   A = 1'b1;
   B = 1'b1;
-  C = 1'b0;
+  Cin = 1'b0;
   #20;
   A = 1'b1;
   B = 1'b1;
-  C = 1'b1;
+  Cin = 1'b1;
   #20;
   A = 1'b0;
   B = 1'b0;
-  C = 1'b0;
-  
+  Cin = 1'b0;
+
 end
 
 endmodule
