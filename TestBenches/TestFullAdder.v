75c00cccce04b40ae6cd97af1ade462ff75f70a9
Derek Johnston <derek.a.johnston@ttu.edu>
2018-01-25 09:54:03 -0600

    Adding a testbench for the FullAdder Module

---
+++ TestBenches/TestFullAdder.v
@@ -0,0 +1,62 @@
+// TestFullAdder.v
+// Developed By: Derek Johnston
+//
+// Provide a testbench for the Full Adder module.
+module TestFullAdder();
+
+// Registers for the inputs.
+reg A, B, Cin;
+
+// Wires for the outputs.
+wire S, Cout;
+
+// Instantiate the Module
+FullAdder uut (
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .S(S),
+    .Cout(Cout)
+);
+
+// Execute the TestBench
+initial begin
+  A = 1'b0;
+  B = 1'b0;
+  C = 1'b0;
+  #20;
+  A = 1'b0;
+  B = 1'b0;
+  C = 1'b1;
+  #20;
+  A = 1'b0;
+  B = 1'b1;
+  C = 1'b0;
+  #20;
+  A = 1'b0;
+  B = 1'b1;
+  C = 1'b1;
+  #20;
+  A = 1'b1;
+  B = 1'b0;
+  C = 1'b0;
+  #20;
+  A = 1'b1;
+  B = 1'b0;
+  C = 1'b1;
+  #20;
+  A = 1'b1;
+  B = 1'b1;
+  C = 1'b0;
+  #20;
+  A = 1'b1;
+  B = 1'b1;
+  C = 1'b1;
+  #20;
+  A = 1'b0;
+  B = 1'b0;
+  C = 1'b0;
+  
+end
+
+endmodule
